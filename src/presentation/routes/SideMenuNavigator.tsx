f8af9bdea651935fef7709b035c197cb076fe071
Xavi Flores <xa_franc@live.com>
2024-08-20 13:37:53 -0600

    sesion8: tabs materialtabs, material top scrollable tabs

--- src/presentation/routes/SideMenuNavigator.tsx
+++ src/presentation/routes/SideMenuNavigator.tsx
@@ -1,15 +1,53 @@
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import {ProfileScreen} from '../screens/profile/ProfileScreen';
+import {globalColors} from '../theme/theme';
+import {Text, useWindowDimensions, View} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 const SideMenuNavigator = () => {
+  const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'slide',
+        headerShown: false,
+        drawerActiveBackgroundColor: globalColors.primary,
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: globalColors.primary,
+        drawerItemStyle: {
+          borderRadius: 100,
+          paddingHorizontal: 20,
+        },
+      }}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="StackNavigatorsss" component={StackNavigator} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
     </Drawer.Navigator>
   );
 };
 export default SideMenuNavigator;
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: globalColors.primary,
+          margin: 30,
+          borderRadius: 50,
+        }}
+      />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,28 @@
 import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
-import {type NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  type NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import PrimaryButton from '../../components/shared/PrimaryButton';
 import {type RootStackParams} from '../../routes/StackNavigator';
+import {useEffect} from 'react';
 
 const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
   return (
     <View style={globalStyles.container}>
       <PrimaryButton
